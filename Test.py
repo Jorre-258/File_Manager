a1c1388060195ef5e1f89ecb133dd3b062c0e3fc
Jorre-258 <130450962+Jorre-258@users.noreply.github.com>
2025-01-23 11:48:54 +0100

    Add files via upload
    23/01/25

--- Test.py
+++ Test.py
@@ -1,10 +1,34 @@
+import os
+
+#Functions
 def Intro_Terminal():
-    text = "\nThis is the test file of this project!\n"
+    text = "\nThis is the test file of this project!\n\nOutput of code:"
     print(text)
 
+def Make_Dir(path, name):
+    try: 
+        os.chdir(path)
+    except FileNotFoundError:
+        print("Did not find this directory")
+    except:
+        print("There has been an error")
+
+    try:
+        str(name)
+        os.mkdir(name)
+        print("Succesfully made this directory:", name)
+    except FileExistsError:
+        print("This file already exists")
+    except:
+        print("There has been an error")
+
+
+#Constants
+#Variables
+testmapjes_path = "C:/Users/JoranDelcroix/PycharmProjects/Engineering/File_Manager/Testmapjes"
+dir_name = "testmap1"
+
+#Main code
 Intro_Terminal()
 
-x = 1
-if x==1: 
-    print("TestTest")
-
+Make_Dir(testmapjes_path, dir_name)
